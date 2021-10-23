3683b7dbf0762789e8f21654f8005f7a8f48fb3c
Nigel Phan <nigel.phan11@gmail.com>
2021-10-23 14:29:19 +1300

    Added collapsible navbar

---
+++ app.js
@@ -0,0 +1,14 @@
+// TODO: IMPLEMENT collapsable navbar
+
+
+// TODO: IMPLEMENT button for extending the about section
+
+// COLLAPSIBLE NAVBAR
+const navIcon = document.getElementsByClassName('nav-icon')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+navIcon.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+})
+// TODO: ADD regex to email input field in contact form
+
