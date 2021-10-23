4d8fadf237d4e47174364f887c0c7b0a15d45d98
Nigel Phan <nigel.phan11@gmail.com>
2021-10-24 04:04:16 +1300

    Changed styling of navbar, Added logic to navbar in mobile view

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // TODO: IMPLEMENT collapsable navbar
 
-
 // TODO: IMPLEMENT button for extending the about section
 
 // COLLAPSIBLE NAVBAR
-const navIcon = document.getElementsByClassName('nav-icon')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const navIcon = document.getElementsByClassName("nav-icon")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-navIcon.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-})
+// Expand navbar
+navIcon.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+// Hide navbar after clicking a tab
+navbarLinks.addEventListener("click", () => {
+  navbarLinks.classList.remove("active");
+});
+
 // TODO: ADD regex to email input field in contact form
-
