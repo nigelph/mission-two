b3553e8b6606f005726cb8adfa20c31971d3c19e
Nigel Phan <nigel.phan11@gmail.com>
2021-10-25 15:25:41 +1300

    Refined JS navbar code, changed colours  of header and navbar

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-// TODO: IMPLEMENT collapsable navbar
-
 // TODO: IMPLEMENT button for extending the about section
 
 // COLLAPSIBLE NAVBAR
 const navIcon = document.getElementsByClassName("nav-icon")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-// Expand navbar
-navIcon.addEventListener("click", () => {
+// Expand or collapse navbar when clicking the navbar icon
+function expandNav(e) {
   navbarLinks.classList.toggle("active");
-});
-// Hide navbar after clicking a tab
-navbarLinks.addEventListener("click", () => {
-  navbarLinks.classList.remove("active");
-});
+  console.log(e);
+}
 
+// Hide navbar after clicking a tab
+function collapseNavOnClick(e) {
+  navbarLinks.classList.remove("active");
+}
+
+navIcon.addEventListener("click", expandNav);
+navbarLinks.addEventListener("click", collapseNavOnClick);
 // TODO: ADD regex to email input field in contact form
