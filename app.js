3f557f5899f33a80abfc15c5a21f2e50a2f4e39f
Nigel Phan <nigel.phan11@gmail.com>
2021-11-03 00:44:24 +1300

    added in expandable about website section. Updated JS, CSS code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// TODO: IMPLEMENT button for extending the about section
 const aboutWebsiteButtton = document.getElementById("about-website-button");
-const aboutSection = document.getElementsByClassName("about-website")[0];
 const arrow = document.getElementsByClassName("fas fa-chevron-down fa-4x")[0];
 
 function expandAbout() {
+  const aboutSection = document.getElementsByClassName("about-website")[0];
+
   aboutSection.classList.toggle("expand-about");
 }
 
@@ -11,6 +11,8 @@ function changeArrow() {
   if (arrow.classList.contains("fa-chevron-down")) {
     arrow.classList.replace("fa-chevron-down", "fa-chevron-up");
   } else {
+    arrow.classList.transition = "10s ease-in-out";
+
     arrow.classList.replace("fa-chevron-up", "fa-chevron-down");
   }
 }
@@ -24,12 +26,10 @@ aboutWebsiteButtton.addEventListener("click", () => {
 const navIcon = document.getElementsByClassName("nav-icon")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-// Expand or collapse navbar when clicking the navbar icon
 function expandNav() {
   navbarLinks.classList.toggle("active");
 }
 
-// Hide navbar after clicking a tab
 function collapseNavOnClick() {
   navbarLinks.classList.remove("active");
 }
@@ -37,6 +37,38 @@ function collapseNavOnClick() {
 navIcon.addEventListener("click", expandNav);
 navbarLinks.addEventListener("click", collapseNavOnClick);
 // TODO: ADD regex to email input field in contact form
-for (var i = 0; i < arrow.length; i++) {
-  console.log(arrow[i]);
+
+const userHover = document.getElementsByClassName("fab fa-github-square fa-9x");
+const firstText = document.querySelectorAll(".about-website-img-box-title")[0];
+const secondText = document.querySelectorAll(".about-website-img-box-title")[1];
+
+function toggleOpacity(event) {
+  // First
+  if ((event.type = "mouseoever" && event.target.innerText === "Version 1")) {
+    event.target.style.opacity = 0.5;
+    event.target.style.transition = "0.8s ease";
+    firstText.classList.add("show-title");
+  }
+  if (event.type === "mouseout" && event.target.innerText === "Version 1") {
+    event.target.style.opacity = 1;
+    event.target.style.transition = "0.8s ease";
+    firstText.classList.remove("show-title");
+  }
+  // Second
+  if ((event.type = "mouseoever" && event.target.innerText === "Version 2")) {
+    event.target.style.opacity = 0.5;
+    event.target.style.transition = "0.8s ease";
+    secondText.classList.add("show-title");
+  }
+  if (event.type === "mouseout" && event.target.innerText === "Version 2") {
+    event.target.style.opacity = 1;
+    event.target.style.transition = "0.8s ease";
+    secondText.classList.remove("show-title");
+  }
 }
+
+userHover[0].addEventListener("mouseover", toggleOpacity);
+userHover[0].addEventListener("mouseout", toggleOpacity);
+
+userHover[1].addEventListener("mouseover", toggleOpacity);
+userHover[1].addEventListener("mouseout", toggleOpacity);
