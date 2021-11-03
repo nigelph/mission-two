33607d091ba44de66edad1b829d790a55123232d
Nigel Phan <nigel.phan11@gmail.com>
2021-11-03 22:14:19 +1300

    cleaned up code

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+// Collapsible navbar variables
+const navIcon = document.getElementsByClassName("nav-icon")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+// About website icon hover variable
+const userHover = document.getElementsByClassName("fab fa-github-square fa-9x");
+// Change about website arrow button variables
 const aboutWebsiteButtton = document.getElementById("about-website-button");
 const arrow = document.getElementsByClassName("fas fa-chevron-down fa-4x")[0];
 
+// COLLAPSIBLE NAVBAR
+function expandNav() {
+  navbarLinks.classList.toggle("active");
+}
+
+function collapseNavOnClick() {
+  navbarLinks.classList.remove("active");
+}
+
+navIcon.addEventListener("click", expandNav);
+navbarLinks.addEventListener("click", collapseNavOnClick);
+
+// Toggle expand button for about website section
 function expandAbout() {
   const aboutSection = document.getElementsByClassName("about-website")[0];
 
@@ -22,27 +41,15 @@ aboutWebsiteButtton.addEventListener("click", () => {
   changeArrow();
 });
 
-// COLLAPSIBLE NAVBAR
-const navIcon = document.getElementsByClassName("nav-icon")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-function expandNav() {
-  navbarLinks.classList.toggle("active");
-}
-
-function collapseNavOnClick() {
-  navbarLinks.classList.remove("active");
-}
-
-navIcon.addEventListener("click", expandNav);
-navbarLinks.addEventListener("click", collapseNavOnClick);
-// TODO: ADD regex to email input field in contact form
-
-const userHover = document.getElementsByClassName("fab fa-github-square fa-9x");
-const firstText = document.querySelectorAll(".about-website-img-box-title")[0];
-const secondText = document.querySelectorAll(".about-website-img-box-title")[1];
-
+// Opacity for img boxes in about website section
 function toggleOpacity(event) {
+  const firstText = document.querySelectorAll(
+    ".about-website-img-box-title"
+  )[0];
+  const secondText = document.querySelectorAll(
+    ".about-website-img-box-title"
+  )[1];
+
   // First
   if ((event.type = "mouseoever" && event.target.innerText === "Version 1")) {
     event.target.style.opacity = 0.5;
@@ -66,7 +73,10 @@ function toggleOpacity(event) {
     secondText.classList.remove("show-title");
   }
 }
+
 for (let index = 0; index < userHover.length; index++) {
   userHover[index].addEventListener("mouseover", toggleOpacity);
   userHover[index].addEventListener("mouseout", toggleOpacity);
 }
+
+// TODO: ADD regex to email input field in contact form
